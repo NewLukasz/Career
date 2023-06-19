91a8f6cf490629de7e04587f6652ca6801020053
Łukasz Kilijański <lukasz@ukaszs-Mac-mini.local>
2023-06-19 13:10:32 +0200

    3 - Add ViewModel for form

---
+++ ViewModel/Application/FormViewModel.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace STLK\Career\ViewModel\Application;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Framework\App\RequestInterface;
+use STLK\Career\Model\Config\ConfigProvider;
+
+class FormViewModel implements ArgumentInterface
+{
+    /**
+     * @param RequestInterface $request
+     * @param ConfigProvider $configProvider
+     */
+    public function __construct(
+        private RequestInterface $request,
+        private ConfigProvider $configProvider
+    )
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function getOpenPositions() : array
+    {
+        return $this->configProvider->getOpenPositions();
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkIfFormSubmittedWithSuccess() : bool
+    {
+        $params = $this->request->getParams();
+        if(!isset($params['result'])){
+            return false;
+        }
+        return $params['result']==='success';
+    }
+
+    /**
+     * @param string $stringWithSpaces
+     * @return string
+     */
+    public function changeSpacesToUnderscoreForDivValue(string $stringWithSpaces) : string
+    {
+        return str_replace(' ','_',$stringWithSpaces);
+    }
+}
